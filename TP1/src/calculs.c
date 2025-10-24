1996f6b49126c3fdee8f539f912210b06f2ae0f9
sofiabouraza-source <sofia.bouraza@gmail.com>
2025-10-24 10:46:27 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;   // Premier entier
+    int num2 = 3;    // Deuxième entier
+    char op = '+';   // Opérateur choisi: '+', '-', '*', '/', '%', '&', '|', '~'
+
+    int resultat;    // Variable pour stocker le résultat
+
+    switch(op) {
+        case '+':
+            resultat = num1 + num2;
+            printf("%d + %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '-':
+            resultat = num1 - num2;
+            printf("%d - %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '*':
+            resultat = num1 * num2;
+            printf("%d * %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                resultat = num1 / num2;
+                printf("%d / %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : division par zéro !\n");
+            }
+            break;
+
+        case '%':
+            if (num2 != 0) {
+                resultat = num1 % num2;
+                printf("%d %% %d = %d\n", num1, num2, resultat);
+            } else {
+                printf("Erreur : modulo par zéro !\n");
+            }
+            break;
+
+        case '&':
+            resultat = num1 & num2;
+            printf("%d & %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '|':
+            resultat = num1 | num2;
+            printf("%d | %
+
 
