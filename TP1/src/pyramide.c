ec395359a17bc750b915de144db3dafa525aef9c
sofiabouraza-source <sofia.bouraza@gmail.com>
2025-10-24 11:47:14 +0200

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n = 5; // Hauteur de la pyramide
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+        // 1. Affichage des espaces pour centrer les nombres
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        // 2. Affichage des nombres croissants de 1 à i
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        // 3. Affichage des nombres décroissants de i-1 à 1
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        // Passage à la ligne suivante après chaque niveau
+        printf("\n");
+    }
+
+    printf("\nGénération de la pyramide terminée.\n");
+
+    return 0;
+}
 
