b90057438847da8be7c8d2ef2c244a7fbc53af9d
sofiabouraza-source <sofia.bouraza@gmail.com>
2025-12-02 11:21:08 +0100

    Update octets.c

--- TP3/src/octets.c
+++ TP3/src/octets.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    short s = 0x0302;
-    int i = 0x04030201;
-    long int l = 0x0807060504030201L;
+    short s = 0x0203;
+    int i = 0x01020304;
+    long int l = 0x0102030405060708L;
     float f = 3.5f;
     double d = 1.0;
     long double ld = 1.0L;
@@ -11,20 +11,47 @@ int main(void) {
     unsigned char *p;
     int k;
 
-    /* short */
     printf("Octets de short :\n");
     p = (unsigned char *)&s;
     for (k = 0; k < (int)sizeof(s); k++) {
-        printf(" %02x", p[k]);
+        printf(" %02x", (unsigned int)p[k]);
     }
     printf("\n\n");
 
-    /* int */
     printf("Octets de int :\n");
     p = (unsigned char *)&i;
     for (k = 0; k < (int)sizeof(i); k++) {
-        printf(" %02x", p[k]);
+        printf(" %02x", (unsigned int)p[k]);
     }
     printf("\n\n");
 
-    /* long int*
+    printf("Octets de long int :\n");
+    p = (unsigned char *)&l;
+    for (k = 0; k < (int)sizeof(l); k++) {
+        printf(" %02x", (unsigned int)p[k]);
+    }
+    printf("\n\n");
+
+    printf("Octets de float :\n");
+    p = (unsigned char *)&f;
+    for (k = 0; k < (int)sizeof(f); k++) {
+        printf(" %02x", (unsigned int)p[k]);
+    }
+    printf("\n\n");
+
+    printf("Octets de double :\n");
+    p = (unsigned char *)&d;
+    for (k = 0; k < (int)sizeof(d); k++) {
+        printf(" %02x", (unsigned int)p[k]);
+    }
+    printf("\n\n");
+
+    printf("Octets de long double :\n");
+    p = (unsigned char *)&ld;
+    for (k = 0; k < (int)sizeof(ld); k++) {
+        printf(" %02x", (unsigned int)p[k]);
+    }
+    printf("\n");
+
+    return 0;
+}
