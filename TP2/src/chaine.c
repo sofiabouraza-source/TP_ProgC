9c92e8a85fd15222d6510bf39b0e2b32e1acf507
sofiabouraza-source <sofia.bouraza@gmail.com>
2025-12-02 10:25:10 +0100

    Update chaine.c

--- TP2/src/chaine.c
+++ TP2/src/chaine.c
@@ -1,25 +1,56 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
     char chaine1[50] = "Hello";
     char chaine2[50] = " World!";
     char copie[50];
     char concat[100];
-    int i, j, longueur = 0;
 
-    /* --- 1) Calcul du nombre de caractères dans chaine1 --- */
+    int i;
+    int j;
+    int longueur;
+
+    /* 1) Calcul de la longueur de chaine1 */
+    longueur = 0;
     i = 0;
     while (chaine1[i] != '\0') {
         longueur++;
         i++;
     }
+
     printf("Longueur de \"%s\" = %d\n", chaine1, longueur);
 
-    /* --- 2) Copie de chaine1 dans copie --- */
+    /* 2) Copie de chaine1 dans copie */
     i = 0;
     while (chaine1[i] != '\0') {
         copie[i] = chaine1[i];
         i++;
     }
-    copie[i] = '\0'; // ajout du caractère de fin
-    printf("Copie de chaine1 :
+    copie[i] = '\0';
+
+    printf("Copie de chaine1 : %s\n", copie);
+
+    /* 3) Concaténation de chaine1 et chaine2 dans concat */
+
+    /* On commence par copier chaine1 dans concat */
+    i = 0;
+    while (chaine1[i] != '\0') {
+        concat[i] = chaine1[i];
+        i++;
+    }
+
+    /* Puis on ajoute chaine2 à la suite */
+    j = 0;
+    while (chaine2[j] != '\0') {
+        concat[i] = chaine2[j];
+        i++;
+        j++;
+    }
+    concat[i] = '\0';
+
+    printf("Concaténation : %s\n", concat);
+
+    return 0;
+}
+
+
