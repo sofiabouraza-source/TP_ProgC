d4ffb9923f60e118664aeabcd29cb4731dc2a981
sofiabouraza-source <sofia.bouraza@gmail.com>
2025-12-02 11:41:13 +0100

    Update operator.c

--- TP4/src/operator.c
+++ TP4/src/operator.c
@@ -1,15 +1,44 @@
 #include "operator.h"
 
-int calcul(int num1, int num2, char op) {
+int calcul(int num1, int num2, char op) 
+{
+    int result = 0;
+
     switch (op) {
-        case '+': return num1 + num2;
-        case '-': return num1 - num2;
-        case '*': return num1 * num2;
-        case '/': return (num2 != 0) ? num1 / num2 : 0; /* éviter division par 0 */
-        case '%': return (num2 != 0) ? num1 % num2 : 0; /* idem */
-        case '&': return num1 & num2; /* ET bit à bit */
-        case '|': return num1 | num2; /* OU bit à bit */
-        case '~': return ~num1;       /* négation binaire */
-        default:  return 0;           /* opérateur inconnu */
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                result = num1 / num2;
+            else
+                result = 0; /* éviter division par zero */
+            break;
+        case '%':
+            if (num2 != 0)
+                result = num1 % num2;
+            else
+                result = 0; /* éviter modulo par zero */
+            break;
+        case '&':
+            result = num1 & num2; /* ET bit à bit */
+            break;
+        case '|':
+            result = num1 | num2; /* OU bit à bit */
+            break;
+        case '~':
+            result = ~num1; /* negation binaire (ignore num2) */
+            break;
+        default:
+            result = 0; /* operateur inconnu */
     }
+
+    return result;
 }
+
