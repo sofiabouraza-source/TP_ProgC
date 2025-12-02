1edc45989a7c609298678869720f29f70c46dad3
sofiabouraza-source <sofia.bouraza@gmail.com>
2025-12-02 10:51:40 +0100

    Update grand_petit.c

--- TP3/src/grand_petit.c
+++ TP3/src/grand_petit.c
@@ -2,23 +2,36 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define TAILLE 100
-
 int main(void) {
-    int tab[TAILLE];
+    int tab[100];
     int i;
     int min, max;
 
-    /* Initialisation du générateur de nombres aléatoires */
-    srand((unsigned int)time(NULL));
+    /* Initialisation du générateur aléatoire */
+    srand(time(NULL));
 
-    /* Remplissage du tableau avec des valeurs entre 1 et 1000 */
-    for (i = 0; i < TAILLE; i++) {
-        tab[i] = (rand() % 1000) + 1; /* valeurs entre 1 et 1000 */
+    /* Remplir le tableau */
+    for (i = 0; i < 100; i++) {
+        tab[i] = rand() % 1000 + 1; /* valeurs 1 à 1000 */
     }
 
-    /* Initialisation de min et max avec la première valeur */
+    /* Initialiser min et max avec la première valeur */
     min = tab[0];
     max = tab[0];
 
-    /* Recherche du plus petit*
+    /* Rechercher min et max */
+    for (i = 1; i < 100; i++) {
+        if (tab[i] < min) {
+            min = tab[i];
+        }
+        if (tab[i] > max) {
+            max = tab[i];
+        }
+    }
+
+    /* Afficher les valeurs */
+    printf("Le numero le plus petit est : %d\n", min);
+    printf("Le numero le plus grand est : %d\n", max);
+
+    return 0;
+}
